0f353c57ee5602cfa2c387251d24c653480a8973
josimar-alves <josimar.alves@dce.ufpb.br>
2018-04-20 17:54:13 -0300

    lol

--- src/teste/Testar.java
+++ src/teste/Testar.java
@@ -63,18 +63,18 @@ public class Testar {
 	@Test
 	public void teste01() throws TestLinkAPIException {
 		int n = 1;
-		sendResult(n == 1, "tc-1", "Falhou 01");
+		sendResult(n == 2, "tc-1", "Falhou 01");
 	}
 	
 	@Test
 	public void teste02() throws TestLinkAPIException {
-		int n = 2;
+		int n = 1;
 		sendResult(n == 2, "tc-2", "Falhou 02");
 	}
 	
 	@Test
 	public void teste03() throws TestLinkAPIException {
-		int n = 2;
+		int n = 1;
 		sendResult(n == 2, "tc-3", "Falhou 03");
 	}
 	
