b7732e303738cb678bdee361d6a70793d328f4ee
josimar-alves <josimar.alves@dce.ufpb.br>
2018-04-23 10:04:18 -0300

    Novo bat

--- Exec.bat
+++ Exec.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\Josimar\eclipse-workspace\Executar
+set projectLocation=C:\Users\Josimar\.jenkins\workspace\Executar
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
